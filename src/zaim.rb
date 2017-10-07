a31ef8eb9e7ec9d3899b7f0b7d5c91afac865421
Sa2Knight <shingo.sasaki.0529@gmail.com>
2017-10-08 00:51:56 +0900

    支出一覧を取得するメソッドを追加

--- src/zaim.rb
+++ src/zaim.rb
@@ -4,6 +4,8 @@ require 'oauth'
 class Zaim
 
   API_URL = 'https://api.zaim.net/v2/'
+  PRIVATE_BUDGET   = 40000
+  PRIVATE_COMMENT  = "私費"
 
   #
   # 初期化時にZaimAPIの利用準備を行う
@@ -15,28 +17,49 @@ class Zaim
       authorize_url: "https://auth.zaim.net/users/auth",
       access_token_path: "https://api.zaim.net"
     }
-    api_key = ENV['ZAIM_API_KEY']
-    api_secret = ENV['ZAIM_API_SECRET']
-    access_key = ENV['ZAIM_ACCESS_KEY']
+    api_key       = ENV['ZAIM_API_KEY']
+    api_secret    = ENV['ZAIM_API_SECRET']
+    access_key    = ENV['ZAIM_ACCESS_KEY']
     access_secret = ENV['ZAIM_ACCESS_SECRET']
     @consumer = OAuth::Consumer.new(api_key, api_secret, oauth_params)
     @access_token = OAuth::AccessToken.new(@consumer, access_key, access_secret)
   end
 
   #
-  # ZaimAPIに対してPOSTリクエストを送信
+  # 支出一覧を取得する
   #
-  def get(url)
-    response = @access_token.get("#{API_URL}#{url}")
-    JSON.parse(response.body)
+  def fetch_payments(opt = {})
+    params = {
+      mode: :payment
+    }
+    url = make_url("home/money", params)
+    @payments = get(url)['money']
+    return self
   end
 
-  #
-  # ZaimAPIに対してPUTリクエストを送信
-  #
-  def put(url , params = nil)
-    response = @access_token.put("#{API_URL}#{url}" , params)
-    JSON.parse(response.body)
-  end
+  private
+
+    #
+    # ZaimAPIに対してGETリクエストを送信
+    #
+    def get(url)
+      response = @access_token.get("#{API_URL}#{url}")
+      JSON.parse(response.body)
+    end
+
+    #
+    # GET用のURLを生成する
+    #
+    def make_url(url , params)
+      params.each do |k , v|
+        if url.index('?').nil?
+          url += "?#{k}=#{v}"
+        else
+          url += "&#{k}=#{v}"
+        end
+      end
+      url_escape = URI.escape(url)
+      return url_escape
+    end
 
 end
